4b512bac7d1cb35e97b303ee7da8075a6169b800
HyeJu Song <songhj8304@gmail.com>
2025-11-17 20:05:00 +0900

    ✨ feat: 간단한 회원가입 API 구현

--- src/main/java/com/example/umc9th/domain/member/entity/Member.java
+++ src/main/java/com/example/umc9th/domain/member/entity/Member.java
@@ -1,7 +1,9 @@
 package com.example.umc9th.domain.member.entity;
 
 import com.example.umc9th.domain.food.entity.MemberFood;
+import com.example.umc9th.domain.member.enums.Address;
 import com.example.umc9th.domain.member.enums.Gender;
+import com.example.umc9th.domain.member.enums.SocialType;
 import com.example.umc9th.domain.term.entity.MemberTerm;
 import com.example.umc9th.domain.mission.entity.MemberMission;
 import com.example.umc9th.domain.review.entity.Review;
@@ -25,20 +27,36 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", length = 3, nullable = false)
+    @Column(name = "name")
     private String name;
 
-    @Column(name = "gender", nullable = false)
+    @Column(name = "gender")
     @Enumerated(EnumType.STRING)
     @Builder.Default
     private Gender gender = Gender.NONE;
 
-    @Column(name = "birth", nullable = false)
+    @Column(name = "birth")
     private LocalDate birth;
 
-    @Column(name = "point", nullable = false)
+    @Column(name = "address")
+    @Enumerated(EnumType.STRING)
+    private Address address;
+
+    @Column(name="detail_address")
+    private String detailAddress;
+
+    @Column(name = "sosial_uid")
+    private String socialUid;
+
+    @Column(name = "social_type")
+    @Enumerated(EnumType.STRING)
+    @Builder.Default
+    private SocialType socialType = SocialType.LOCAL;
+
+    @Column(name = "point")
     @Builder.Default
     private Integer point = 0;
+
 
     @Column(name = "phone_number", length = 20)
     private String phoneNumber;
@@ -49,7 +67,7 @@ public class Member extends BaseEntity {
 
     @Builder.Default
     @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
-    private List<MemberFood> favoriteFoods = new ArrayList<>();
+    private List<MemberFood> memberFood = new ArrayList<>();
 
     @Builder.Default
     @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
